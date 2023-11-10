aad91dcf09cc0cb206187f3f2f7c0eae61161f70
Alessandro Aglietti <alessandro-aglietti@users.noreply.github.com>
2023-11-10 14:49:41 +0000

    chore: prettier setup

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-import {createHash} from 'node:crypto';
-import fs from 'node:fs';
-import imageUrls from './imageUrls'
-import axios from 'axios';
-
+import { createHash } from "node:crypto";
+import fs from "node:fs";
+import imageUrls from "./imageUrls";
+import axios from "axios";
 
 async function retrieveImage(imageUrl) {
-    const _ = {
-        _: new Date(),
-        imageUrlSha256: createHash('sha256').update(imageUrl).digest('hex'),
-    };
+  const _ = {
+    _: new Date(),
+    imageUrlSha256: createHash("sha256").update(imageUrl).digest("hex"),
+  };
 
-    try {
-        const response = await axios({
-            method: 'get',
-            url: imageUrl,
-            responseType: 'stream'
-        });
+  try {
+    const response = await axios({
+      method: "get",
+      url: imageUrl,
+      responseType: "stream",
+    });
 
-        _.responseAt = new Date();
-        _.imageFileName = `${_.imageUrlSha256}.${imageUrl.slice(-3)}`;
+    _.responseAt = new Date();
+    _.imageFileName = `${_.imageUrlSha256}.${imageUrl.slice(-3)}`;
 
-        response.data.pipe(fs.createWriteStream(_.imageFileName));
+    response.data.pipe(fs.createWriteStream(_.imageFileName));
 
-        _.pipedAt = new Date();
+    _.pipedAt = new Date();
+  } catch (e) {
+    _.e = e;
+  }
 
-    } catch (e) {
-        _.e = e;
-    }
-
-
-    return _;
+  return _;
 }
 
 async function index() {
-    const _ = await retrieveImage(imageUrls[0].url);
-    console.log(_, new Date());
+  const _ = await retrieveImage(imageUrls[0].url);
+  console.log(_, new Date());
 
-    return 0;
+  return 0;
 }
 
 export default index;
