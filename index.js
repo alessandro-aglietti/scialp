c7ce375b105b8f16f81b6c09045eda51de1eeb26
Alessandro Aglietti <alessandro-aglietti@users.noreply.github.com>
2023-11-10 14:47:11 +0000

    feat: image dumper

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-async function retrieveImage(){
-    const _p = new Promise((resolve) => {
-        setTimeout(() => resolve("that's ok!"), 1000);
-    });
+import {createHash} from 'node:crypto';
+import fs from 'node:fs';
+import imageUrls from './imageUrls'
+import axios from 'axios';
 
-    return _p;
+
+async function retrieveImage(imageUrl) {
+    const _ = {
+        _: new Date(),
+        imageUrlSha256: createHash('sha256').update(imageUrl).digest('hex'),
+    };
+
+    try {
+        const response = await axios({
+            method: 'get',
+            url: imageUrl,
+            responseType: 'stream'
+        });
+
+        _.responseAt = new Date();
+        _.imageFileName = `${_.imageUrlSha256}.${imageUrl.slice(-3)}`;
+
+        response.data.pipe(fs.createWriteStream(_.imageFileName));
+
+        _.pipedAt = new Date();
+
+    } catch (e) {
+        _.e = e;
+    }
+
+
+    return _;
 }
 
-async function index(){
-    const _ = await retrieveImage();
+async function index() {
+    const _ = await retrieveImage(imageUrls[0].url);
     console.log(_, new Date());
 
     return 0;
